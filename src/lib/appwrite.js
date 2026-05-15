603608e2fe06d9e2a7c344c5909b182ba61b31eb
annamoyosore <annamoyosore@gmail.com>
2026-05-15 12:58:10 +0100

    Update appwrite.js

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -3,14 +3,20 @@ import { Client, Account, Databases, ID, Query } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("YOUR_PROJECT_ID");
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
+
 export const databases = new Databases(client);
 
 export { ID, Query };
 
-export const DATABASE_ID = "forex";
-export const WALLET_COLLECTION = "wallets";
-export const TRADE_COLLECTION = "trades";
+export const DATABASE_ID =
+  import.meta.env.VITE_DATABASE_ID;
+
+export const WALLET_COLLECTION =
+  import.meta.env.VITE_WALLET_COLLECTION;
+
+export const TRADE_COLLECTION =
+  import.meta.env.VITE_TRADE_COLLECTION;
