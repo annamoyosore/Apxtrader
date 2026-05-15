9dcc08abb5833809909fd1d7d5eaf9a85608b370
annamoyosore <annamoyosore@gmail.com>
2026-05-15 13:00:41 +0100

    Update appwrite.js

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -3,7 +3,7 @@ import { Client, Account, Databases, ID, Query } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setEndpoint("https://nyc.cloud.appwrite.io/v1")
   .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
