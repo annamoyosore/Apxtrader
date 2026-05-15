70fcc6958ea565a6938321cf553893f5a7edfda0
annamoyosore <annamoyosore@gmail.com>
2026-05-15 12:44:43 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="nav">
-      <h2>FOREX DEMO</h2>
+      <h2>Apex Trader</h2>
 
       <div className="links">
         <Link to="/">Dashboard</Link>
