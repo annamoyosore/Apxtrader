4944382864ce379810ef7b9274606189d9ac84b0
annamoyosore <annamoyosore@gmail.com>
2026-05-17 14:32:43 +0100

    Update appwrite.js

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -7,13 +7,12 @@ client
   .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
-
 export const databases = new Databases(client);
 
 export { ID, Query };
 
-export const DATABASE_ID =
-  import.meta.env.VITE_DATABASE_ID;
+// DATABASE CONFIG
+export const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 
 export const WALLET_COLLECTION =
   import.meta.env.VITE_WALLET_COLLECTION;
