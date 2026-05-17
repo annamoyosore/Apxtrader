666699ff145e0fa5a1bfc87ef844159f46e26e78
annamoyosore <annamoyosore@gmail.com>
2026-05-17 14:50:12 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,27 @@
-import { Link } from "react-router-dom";
-import { logoutUser } from "../auth";
+import { Link, useNavigate } from "react-router-dom";
+import { account } from "../lib/appwrite";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    try {
+      await account.deleteSession("current");
+      navigate("/"); // go back to login
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
   return (
     <nav className="navbar">
       <h2>ApexTrader</h2>
 
       <div>
-        <Link to="/">Dashboard</Link>
+        <Link to="/dashboard">Dashboard</Link>
         <Link to="/wallet">Wallet</Link>
 
-        <button onClick={logoutUser}>
+        <button onClick={logout} style={{ marginLeft: "10px" }}>
           Logout
         </button>
       </div>
