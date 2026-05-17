2a54fb0d84ceeb25570f00cee9b71bafbeb6f45a
annamoyosore <annamoyosore@gmail.com>
2026-05-17 14:48:02 +0100

    Update TradingChart.jsx

--- src/components/TradingChart.jsx
+++ src/components/TradingChart.jsx
@@ -1,13 +1,13 @@
 export default function TradingChart() {
   return (
     <iframe
+      title="Trading Chart"
       src="https://s.tradingview.com/widgetembed/?symbol=FX:EURUSD&interval=1&theme=dark"
       style={{
         width: "100%",
         height: "100%",
         border: "none",
       }}
-      title="TradingView Chart"
     />
   );
 }
