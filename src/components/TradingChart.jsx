2144836ba46565c6c376abaa25c83e60c37a5871
annamoyosore <annamoyosore@gmail.com>
2026-05-17 14:45:09 +0100

    Update TradingChart.jsx

--- src/components/TradingChart.jsx
+++ src/components/TradingChart.jsx
@@ -1,11 +1,13 @@
-import TradingViewWidget from "react-tradingview-widget";
-
 export default function TradingChart() {
   return (
-    <TradingViewWidget
-      symbol="FX:EURUSD"
-      theme="dark"
-      autosize
+    <iframe
+      src="https://s.tradingview.com/widgetembed/?symbol=FX:EURUSD&interval=1&theme=dark"
+      style={{
+        width: "100%",
+        height: "100%",
+        border: "none",
+      }}
+      title="TradingView Chart"
     />
   );
 }
